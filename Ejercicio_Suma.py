b1b83a8d07ed149cf8870561eb4a9532f0e8d2d3
SantiagoMRodas <santimosquera958@gmail.com>
2023-02-20 07:39:37 -0500

    raiaqa

--- Ejercicio_Suma.py
+++ Ejercicio_Suma.py
@@ -1,3 +1,21 @@
-Suma = lambda a,b : a + b 
 
-print(Suma(4,8))
+Suma = lambda a,b : a + b 
+Suman=(Suma(4,8))
+
+Resta = lambda a,b : a - b 
+Restas = (Resta(4,8))
+
+Division = lambda a,b : a / b 
+Divisions = (Division(4,8))
+
+Multiplicacion = lambda a,b : a * b 
+Multiplicacions= (Multiplicacion(4,8))
+
+Potencia = lambda a,b : a ** b 
+Potencias = (Potencia(4,8))
+
+Raiz_Cuadrada = lambda a : a **(0/5) 
+Raiz = (Raiz_Cuadrada(4))
+
+results = ('Estos son los resultados de las operaciones Suman = {0} Restas ={1} Divisions = {2} Multiplicacions= {3} Potencias= {4} Raiz= {5}'.format(Suman,Restas,Divisions,Multiplicacions,Potencias,Raiz))
+print(results)
